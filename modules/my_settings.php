213843abd2f870559c28fbb723b8ad8647b1bd5e
Kirill Afan <kirka121@gmail.com>
2013-03-17 11:48:45 -0400

    example of structure of control panel

--- modules/my_settings.php
+++ modules/my_settings.php
@@ -1,1 +1,19 @@
-my_settings
+<link rel="stylesheet" href="assets/css/my_settings.css">
+<div id="my_settings">
+	<a href="index.php?op=my_settings&cp=user_edit">change password</a> | <a href="index.php?op=my_settings&cp=user_info">update personal information</a> | <a href="index.php?op=my_settings&cp=forgot_password">forgot pass</a> | <a href="index.php?op=my_settings&cp=upload_profile_picture">update profile picture</a>
+	<br />
+
+
+	<?php
+	if(!isset($_GET['cp']) || $_GET['cp'] == ""){
+		include("user/home.php"); 
+	} else {
+		$req_page = trim($_GET['cp']);
+		if (is_file("modules/user/".$req_page.".php")) {
+	      	include("modules/user/".$req_page.".php");
+	    } else {	
+			echo ("<div id='error'>Module could not be found!<br/></div>");
+	    }
+	}
+	?>
+</div>
---
+++ modules/user/upload_profile_picture.php
@@ -0,0 +1,1 @@
+uplaod picture here
