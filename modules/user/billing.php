a121a4893f2e38500791e1c2478fd74c1992f54f
Kirill Afan <kirka121@gmail.com>
2013-03-19 17:46:56 -0400

    added billing

--- modules/user/billing.php
+++ modules/user/billing.php
@@ -1,5 +1,69 @@
-available plans<br/>
---5$/month<br/>
---50$/year<br/>
---10$/month<br/>
---100$/year<br/>
+<link rel="stylesheet" href="assets/css/user/billing.css">
+
+<table id="my_settings_container">
+	<tr>
+		<th colspan="100%">Payment Plans</th>
+	</tr>
+	<tr>
+		<td class="title">
+			<table id="payment_plans">
+				<tr>
+					<th>Plans</th>
+					<th>Price</th>
+					<th>Benefits</th>
+					<th align="center"></th>
+				</tr>
+				<tr>
+					<td>Gold +</td>
+					<td>$100 / year</td>
+					<td>0% / transaction charge</td>
+					<td align="center">
+						<p class="submit_button">
+					      <input type="hidden" name="sub_update_plan" value="4">
+					      <input class="grey_button" type="submit" value="Upgrade">
+					    </p>
+					</td>
+				</tr>
+				<tr>
+					<td>Gold</td>
+					<td>$5 / month</td>
+					<td>0% / transaction charge</td>
+					<td align="center">
+						<p class="submit_button">
+					      <input type="hidden" name="sub_update_plan" value="3">
+					      <input class="grey_button" type="submit" value="Upgrade">
+					    </p>
+					</td>
+				</tr>
+				<tr>
+					<td>Silver +</td>
+					<td>$50 / year</td>
+					<td>7% / transaction charge</td>
+					<td align="center">
+						<p class="submit_button">
+					      <input type="hidden" name="sub_update_plan" value="2">
+					      <input class="grey_button" type="submit" value="Upgrade">
+					    </p>
+					</td>
+				</tr>
+				<tr>
+					<td>Silver</td>
+					<td>$10 / month</td>
+					<td>7	% / transaction charge</td>
+					<td align="center">
+						<p class="submit_button">
+					      <input type="hidden" name="sub_update_plan" value="1">
+					      <input class="grey_button" type="submit" value="Upgrade">
+					    </p>
+					</td>
+				</tr>
+				<tr>
+					<td>free</td>
+					<td>$0 / month</td>
+					<td>12% / transaction charge</td>
+					<td align="center">you are here</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
--- modules/user/notification_center.php
+++ modules/user/notification_center.php
@@ -1,10 +1,32 @@
-how you receive notifications<br/>
---Gigs<br/>
-----cellphone<br/>
-----email<br/>
-----web<br/>
-<br/>
---Runs<br/>
-----cellphone<br/>
-----email<br/>
-----web<br/>
+<link rel="stylesheet" href="assets/css/user/notification_center.css">
+
+<table id="my_settings_container">
+	<tr>
+		<th colspan="100%">Your Notifications</th>
+	</tr>
+	<tr>
+		<td class="gigs">
+			<div class="title">Gigs:</div><br/>
+			When you post a gig, you will be notified about the gig's activity here.<br /><br />
+			<input type="checkbox" name="notify_by_web">Web | <input type="checkbox" name="notify_by_email">Email | <input type="checkbox" name="notify_by_cell">Cell
+		</td>
+	</tr>
+	<tr>
+		<td><hr /></td>
+	</tr>
+	<tr>
+		<td class="runs">
+			<div class="title">Runs:</div><br/>
+			When you reply to gig posts, you will be notified about these posts here.<br /><br />
+			<input type="checkbox" name="notify_by_web">Web | <input type="checkbox" name="notify_by_email">Email | <input type="checkbox" name="notify_by_cell">Cell
+		</td>
+	</tr>
+	<tr>
+		<td class="left_padding">
+			<p class="submit_button">
+		      <input type="hidden" name="sub_update_notifications" value="1">
+		      <input class="register_button" type="submit" value="Update"> 
+			</p>
+		</td>
+	</tr>
+</table>
