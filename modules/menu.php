b5155abc6bbd38275bfb13ac40c92b219a755817
Kirill Afan <kirka121@gmail.com>
2013-03-19 10:20:45 -0400

    styling menu, finalizing control panel layout

--- modules/menu.php
+++ modules/menu.php
@@ -1,26 +1,27 @@
 <link rel="stylesheet" href="assets/css/menu.css">
-<div id="menu">
-	<div id="menu_header">
-		<?php
-			if($session->logged_in){
-				if(isset($session->username)){
-					?><img src="service/getimage.php?u=<?=$session->username;?>" width="25" height="25"><?php
-					echo $session->username;
+<div class="border">
+	<table id="menu">
+		<tr>
+			<?php
+				if($session->logged_in){
+					if(isset($session->username)){
+						?>
+						<th class="picture"><img src="service/getimage.php?u=<?php echo $session->username;?>" width="25" height="25"></th>
+						<th class="username"><?php echo $session->username;?></th><?php
+					}
 				}
-			}
-		?>
-	</div>
-	<?php if(isset($_GET['op']) && $_GET['op'] =="my_settings"){ ?>
-		<a href="index.php?op=my_settings&cp=user_edit">Profile</a> <br/>
-		<a href="index.php?op=my_settings&cp=user_info&user=<?php echo $session->username; ?>">Account Settings</a> <br/>
-		<a href="index.php?op=my_settings&cp=forgot_password">Emails</a> <br/>
-		<a href="index.php?op=my_settings&cp=upload_profile_picture">Notification Center</a>
-		<a href="#">Billing</a>
-		<a href="#">Payment History</a>
-	<?php }else{ ?>
-		placeholder for more menu
-	<?php } ?>
-
-
+			?>
+		</tr>
+		<?php if(isset($_GET['op']) && $_GET['op'] =="my_settings"){ ?></td></tr>
+			<tr><td colspan="100%"><a class="inner_link"href="index.php?op=my_settings&cp=user_edit"><div class="link">Profile</div></a></td></tr>
+			<tr><td colspan="100%"><a class="inner_link"href="index.php?op=my_settings&cp=user_info&user=<?php echo $session->username; ?>"><div class="link">Account Settings</div></a></td></tr>
+			<tr><td colspan="100%"><a class="inner_link"href="index.php?op=my_settings&cp=forgot_password"><div class="link">Emails</div></a></td></tr>
+			<tr><td colspan="100%"><a class="inner_link"href="index.php?op=my_settings&cp=upload_profile_picture"><div class="link">Notification Center</div></a></td></tr>
+			<tr><td colspan="100%"><a class="inner_link"href="#"><div class="link">Billing</div></a></td></tr>
+			<tr><td colspan="100%"><a class="inner_link"href="#"><div class="link">Payment History</div></a></td></tr>
+		<?php }else{ ?>
+			<tr><td class="link" colspan="100%"><div class="link">placeholder for more menu</div></td></tr>
+		<?php } ?>
+	</table>
 </div>
 
