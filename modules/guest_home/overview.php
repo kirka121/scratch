d2febfc9971a1034bc6c83746a440ad5d56fa2e0
Kirill Afan <kirka121@gmail.com>
2013-03-23 10:26:15 -0400

    guest_home -> overview done

--- modules/guest_home/overview.php
+++ modules/guest_home/overview.php
@@ -1,1 +1,37 @@
-overview
+<link rel="stylesheet" href="assets/css/guest_home/overview.css">
+<script src="assets/js/overview_showHide.js" type="text/javascript"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+	   $('.show_hide2').showHide2({			 
+			speed: 500,  // speed you want the toggle to happen	
+			changeText: 1, // if you dont want the button text to change, set this to 0
+			showText: 'Login / Register',// the button text to show when a div is closed
+			hideText: 'Login / Register' // the button text to show when a div is open				 
+		}); 
+	});
+</script>
+
+<div id="overview_container">
+	<h1>Get things done by trusted people in your community</h1>
+	<div id="overview_header" class="show_hide2" rel="#slider1">
+		<img src="assets/images/icon-number-1.png">Post a Gig<img width="20" height="20" class="arrow" src="assets/images/arrow-down.png">
+	</div>
+	<div id="slider1">
+		<p><strong>Use a descriptive task title</strong>, tell us the who, what, when and how much you're willing to pay to get your Gig done. Choose to pay by Paypal or Credit card and remember you only pay when the task is completed.</p>
+		<p><strong>As our GigRunners interact</strong> with your task, please respond with more information. For your own safety, only exchange your address, email with the runner you assign.</p>
+	</div>
+	<div id="overview_header" class="show_hide2" rel="#slider2">
+		<img src="assets/images/icon-number-2.png">Review incoming bids<img width="20" height="20" class="arrow" src="assets/images/arrow-down.png">
+	</div>
+	<div id="slider2">
+		<p>GigRunners who want to complete your task will place bids or offers, you can then accept any bid or offer you think is fair to assign that runner to the Gig. <strong>Browse the GigRunner's profile</strong> and check their ratings, reviews to ensure you choose the most qualified Runner for your Gig.</p>
+		<p><strong>You will be notified of new bids</strong> by the means you have specified in your Account Settings</p>
+	</div>
+	<div id="overview_header" class="show_hide2" rel="#slider3">
+		<img src="assets/images/icon-number-3.png">Get gig done<img width="20" height="20" class="arrow" src="assets/images/arrow-down.png">
+	</div>
+	<div id="slider3">
+		<p><strong>Once you have assigned a GigRunner</strong>, you will gain access to private messaging for exchanging private information. When your Gig is completed, you mark the Gig complete and the GigRunner gets paid.</p> 
+		<p><strong>Review the GigRunner</strong> when the jobs done. That way you get to show your appreciation and other GigPosters can choose to use the same Gigrunner you've used based on your recommendation.</p>
+	</div>
+</div>
