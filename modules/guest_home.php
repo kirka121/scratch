67583be9eb452e5e1610ca41a1f7cc7e1872c44d
Kirill Afan <kirka121@gmail.com>
2013-03-23 11:40:40 -0400

    added slider

--- modules/guest_home.php
+++ modules/guest_home.php
@@ -1,5 +1,15 @@
 <link rel="stylesheet" href="assets/css/guest_home.css">
-
+<link href="assets/slider/js-image-slider.css" rel="stylesheet" type="text/css" />
+<script src="assets/slider/js-image-slider.js" type="text/javascript"></script>
+<div id="sliderFrame">
+	<div id="slider">
+	    <img src="assets/slider/image-slider-1.jpg" alt="" />
+	    <img src="assets/slider/image-slider-2.jpg" alt="" />
+	    <img src="assets/slider/image-slider-3.jpg" alt="" />
+	    <img src="assets/slider/image-slider-4.jpg" alt="" />
+	    <img src="assets/slider/image-slider-5.jpg" alt="" />
+	</div>
+</div>
 <div id="guest_home_container">
 	<?php
 	if(!isset($_GET['gh']) || $_GET['gh'] == ""){
